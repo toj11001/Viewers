3cbb36462745195c7aa22088125afc5bab60e3a2
Tobias Joschko <tobias.joschko@sap.com>
2020-10-19 22:29:25 +0200

    updaete config for 34.107.27.6

---
+++ platform/viewer/.recipes/OpenResty-Orthanc-Keycloak/config/viewer.js
@@ -0,0 +1,43 @@
+window.config = {
+  routerBasename: '/',
+  showStudyList: true,
+  servers: {
+    // This is an array, but we'll only use the first entry for now
+    dicomWeb: [
+      {
+        name: 'Orthanc',
+        wadoUriRoot: 'https://34.107.27.6/orthanc/wado',
+        qidoRoot: 'https://34.107.27.6/orthanc/dicom-web',
+        wadoRoot: 'https://34.107.27.6/orthanc/dicom-web',
+        qidoSupportsIncludeField: false,
+        imageRendering: 'wadors',
+        thumbnailRendering: 'wadors',
+        // REQUIRED TAG:
+        // TODO: Remove tag after https://github.com/OHIF/ohif-core/pull/19 is merged and we bump version
+        requestOptions: {
+          // undefined to use JWT + Bearer auth
+          auth: 'admin:$grc.2020!',
+          logRequests: true,
+          logResponses: false,
+          logTiming: true,
+        },
+      },
+    ],
+  },
+  // This is an array, but we'll only use the first entry for now
+  oidc: [
+    {
+      // ~ REQUIRED
+      // Authorization Server URL
+      authority: 'https://dicom.eu.auth0.com/',
+      client_id: 'bwCfAW9uQSA4kW9dzMBNzzJdMWNslxSy',
+      client_secret:
+        'gzv5RZwbPpSeIVgUzMszx24NlK8R8oZObveY67V-PzcmfmVRJmx_-QDGyXbjftMv',
+      redirect_uri: 'https://34.107.27.6/callback', // `OHIFStandaloneViewer.js`
+      response_type: 'code', // "Authorization Code Flow"
+      scope: 'openid email profile', // email profile openid
+      // ~ OPTIONAL
+      post_logout_redirect_uri: '/logout-redirect.html',
+    },
+  ],
+};
